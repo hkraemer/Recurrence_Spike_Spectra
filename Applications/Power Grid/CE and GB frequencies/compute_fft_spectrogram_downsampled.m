dd5b4b70f4db74e511fa967818a3822ad3817706
K.H. Kraemer <hkraemer@pik-potsdam.de>
2022-07-15 17:23:20 +0200

    Power grid: proper Figures added

--- Applications/Power Grid/CE and GB frequencies/compute_fft_spectrogram_downsampled.m
+++ Applications/Power Grid/CE and GB frequencies/compute_fft_spectrogram_downsampled.m
@@ -1,14 +1,17 @@
+% In this script we 1) downsample the original data and 2) compute
+% FFT-spectrograms on these downsampled time series.
+
 clear, clc
 
 f_ce = load("./data/frequencies_ce.mat").frequencies_ce;
 f_gb = load("./data/frequencies_gb.mat").frequencies_gb;
 
-%% Downsample to a sampling time of one minute
+%% Downsample to a sampling time of one minute/ 20 sec / 30 sec
 
-T_ce = 0.2; % sampling time in secs
+T_ce = 0.2; % original sampling time in secs
 T_gb = 1;
 
-T_ce_ds = 20; % new sampling time
+T_ce_ds = 20; % new sampling time 20 s
 T_gb_ds = 20;
 
 factor_ce = 1/T_ce * T_ce_ds;
@@ -23,13 +26,6 @@ L_gb_ds = length(f_gb_ds);
 t_ce = (0:L_ce_ds-1)*T_ce_ds; % time vector
 t_gb = (0:L_gb_ds-1)*T_gb_ds;
 
-% % checking
-% subplot(211)
-% plot(t_ce(1:21000), f_ce(1:21000), '.-')
-% grid on
-% subplot(212)
-% plot(t_ce_ds(1:21000/factor_ce), f_ce_ds(1:21000/factor_ce), '.-')
-% grid on
 
 %% Short time FFT
 
@@ -41,8 +37,8 @@ Fs_gb = 1/T_gb;
 L_ce = length(f_ce); % length of signal
 L_gb = length(f_gb);
 
-L_block_ce = 1440; % block length covering one full day (24 hours)
-L_block_gb = 1440;
+L_block_ce = 4320; % block length covering one full day (24 hours)
+L_block_gb = 4320;
 
 t_ce = (0:L_block_ce-1)*T_ce; % time vector for block
 t_gb = (0:L_block_gb-1)*T_gb;
@@ -62,98 +58,14 @@ toc
 spec_ce = mean(spectrum_ce,2);
 spec_gb = mean(spectrum_gb,2);
 
-%% Plotting
+%% Save results 
 
-% iss_spec_ce = load("./data/iss_spec_ce_cao_0_8_0_5.csv");
-% iss_spec_gb = load("./data/iss_spec_gb_cao_0_8_0_5.csv");
-% iss_spec_ce = load("./data/iss_spec_ce_hegger_0_8_0_5.csv");
-% iss_spec_gb = load("./data/iss_spec_gb_hegger_0_8_0_5.csv");
-% iss_spec_ce = load("./data/iss_spec_ce_hegger_0_9_0_5.csv");
-% iss_spec_gb = load("./data/iss_spec_gb_hegger_0_9_0_5.csv");
-% iss_spec_ce = load("./data/iss_spec_ce_standard_60_0_9_0_5.csv");
-% iss_spec_gb = load("./data/iss_spec_gb_standard_60_0_9_0_5.csv");
-% iss_spec_ce = load("./data/iss_spec_ce_cao_60_0_95_0_8.csv");
-% iss_spec_gb = load("./data/iss_spec_gb_cao_60_0_95_0_8.csv");
-% iss_spec_ce = load("./data/iss_spec_ce_cao_60_0_99_0_8.csv");
-% iss_spec_gb = load("./data/iss_spec_gb_cao_60_0_99_0_8.csv");
-% iss_spec_ce = load("./data/iss_spec_ce_cao_STLS_60_0_95_0_8.csv");
-% iss_spec_gb = load("./data/iss_spec_gb_cao_STLS_60_0_95_0_8.csv");
-% iss_spec_ce = load("./data/iss_spec_ce_standard_60_0_95_0_8.csv");
-% iss_spec_gb = load("./data/iss_spec_gb_standard_60_0_95_0_8.csv");
-iss_spec_ce = load("./data/iss_spec_ce_standard_20_0_95_0_8.csv");
-iss_spec_gb = load("./data/iss_spec_gb_standard_20_0_95_0_8.csv");
+% save("./data/frequencies_ce_downsampled_20s.csv", "f_ce_ds", "-ascii")
+% save("./data/frequencies_gb_downsampled_20s.csv", "f_gb_ds", "-ascii")
 
-%%
-t = (1:300)*0.333333333333333;
+save("./data/fft_spec_ce.csv","spec_ce","-ascii")
+save("./data/fft_spec_gb.csv","spec_ce","-ascii")
 
-figure
-subplot(221)
-semilogx(1./fs_ce, spec_ce)
-title('Single-Sided Amplitude Spectrum of F_{CE}')
-xline(15,'r--')
-xline(30,'r--')
-xline(60,'r--')
-xlabel('period (min)')
-ylabel('|P1(f)|')
-xlim([1 100])
-grid on
-
-
-subplot(222)
-semilogx(1./fs_gb, spec_gb)
-title('Single-Sided Amplitude Spectrum of F_{GB}')
-xline(15,'r--')
-xline(30,'r--')
-xline(60,'r--')
-xlabel('period (min)')
-ylabel('|P1(f)|')
-xlim([1 100])
-grid on
-
-subplot(223)
-semilogx(t, iss_spec_ce(1:300))
-title('ISS of F_{CE}')
-xline(15,'r--')
-xline(30,'r--')
-xline(60,'r--')
-xlabel('period (min)')
-ylabel('|P1(f)|')
-xlim([1 100])
-ylim([0 0.01])
-grid on
-
-subplot(224)
-semilogx(t, iss_spec_gb(1:300))
-title('ISS of F_{GB}')
-xline(15,'r--')
-xline(30,'r--')
-xline(60,'r--')
-xlabel('period (min)')
-ylabel('|P1(f)|')
-xlim([1 100])
-ylim([0 0.01])
-grid on
-
-%% ISS for downsampled time series
-
-x = f_ce_ds;
-blocksize = L_block_ce;
-
-T = length(x);
-blocks = floor(T/blocksize);
-
-spectrum = zeros(blocksize/2+1, blocks);
-
-i = 1;
-
-time = 1+(i-1)*blocksize:(i*blocksize);
-data = x(time);
-
-[Y, TAU_VALS, TS_VALS] = pecuzal_embedding(data);
-
-%%
-save("frequencies_ce_downsampled_20s.csv", "f_ce_ds", "-ascii")
-save("frequencies_gb_downsampled_20s.csv", "f_gb_ds", "-ascii")
 
 %%
 function spectrum = fft_spec(x, blocksize)
---
+++ Applications/Power Grid/CE and GB frequencies/plot_frequency_results.m
@@ -0,0 +1,128 @@
+% We plot the downsampled time series 
+% (computed in `compute_fft_spectrogram_downsampled`), the FFT-spectrograms 
+% (computed in `compute_fft_spectrogram_downsampled`) and the according ISS 
+% (computed in `compute_iss_spectrogram_downsampled.jl`).
+
+clear, clc
+
+%% Load the computed data
+
+% (downsampled) time series (computed in `compute_fft_spectrogram_downsampled`)
+f_ce = load("./data/frequencies_ce_downsampled_20s.csv");
+f_gb = load("./data/frequencies_gb_downsampled_20s.csv");
+
+% FFT spec (computed in `compute_fft_spectrogram_downsampled`)
+spec_ce = load("./data/fft_spec_ce.csv");
+spec_gb = load("./data/fft_spec_gb.csv");
+
+% ISS spec (computed in `compute_iss_spectrogram_downsampled.jl`)
+iss_spec_ce = load("./data/iss_spec_ce_standard_20_0_95_0_8.csv");
+iss_spec_gb = load("./data/iss_spec_gb_standard_20_0_95_0_8.csv");
+
+%% Set time and frequency vectors
+
+dt_s = 20; % sampling time is sec
+dt_m = 1/ (60/dt_s);
+
+% time/period vector for ISS 
+N = length(iss_spec_ce);
+t = (1:N)*dt_m;
+
+% time/period/frequency vector for FFT spectrogram
+Fs_ce = 60/dt_s; % sampling frequency min^-1
+Fs_gb = 60/dt_s;
+
+L_block_ce = 4320; % block length covering one full day (24 hours, 60/dt_s * 60 * 24))
+L_block_gb = 4320;
+
+t_ce = (0:L_block_ce-1)*dt_m; % time vector for block in min
+t_gb = (0:L_block_gb-1)*dt_m;
+
+fs_ce= Fs_ce*(0:(L_block_ce/2))/L_block_ce; % frequency vector in min^1
+fs_gb= Fs_gb*(0:(L_block_gb/2))/L_block_gb;
+
+%% Plotting
+
+font = 22; % fontsize
+lw1 = 2.5;
+lw2 = 2;
+
+
+figure('Units','normalized','Position',[.001 .001 .999 .999])
+subplot(221)
+semilogx(1./fs_ce, spec_ce, 'LineWidth', lw1)
+title('Single-Sided Amplitude Spectrum of F_{CE}')
+xline(15,'r--','LineWidth', lw1)
+xline(30,'r--','LineWidth', lw1)
+xline(60,'r--','LineWidth', lw1)
+% xlabel('period (min)')
+ylabel('|power/period|')
+xlim([1 100])
+set(gca, 'LineWidth', lw2, 'FontSize', font)
+grid on
+
+
+subplot(222)
+semilogx(1./fs_gb, spec_gb, 'LineWidth', lw1)
+title('Single-Sided Amplitude Spectrum of F_{GB}')
+xline(15,'r--','LineWidth', lw1)
+xline(30,'r--','LineWidth', lw1)
+xline(60,'r--','LineWidth', lw1)
+ylabel('|power/period|')
+xlim([1 100])
+set(gca, 'LineWidth', lw2, 'FontSize', font)
+grid on
+
+subplot(223)
+semilogx(t, iss_spec_ce(1:300), 'LineWidth', lw1)
+title('ISS of \tau-RR of F_{CE}')
+xline(15,'r--','LineWidth', lw1)
+xline(30,'r--','LineWidth', lw1)
+xline(60,'r--','LineWidth', lw1)
+ylabel('|power/period|')
+xlabel('period (min)')
+xlim([1 100])
+ylim([0 0.01])
+set(gca, 'LineWidth', lw2, 'FontSize', font)
+grid on
+
+subplot(224)
+semilogx(t, iss_spec_gb(1:300), 'LineWidth', lw1)
+title('ISS of \tau-RR of F_{GB}')
+xline(15,'r--','LineWidth', lw1)
+xline(30,'r--','LineWidth', lw1)
+xline(60,'r--','LineWidth', lw1)
+ylabel('|power/period|')
+xlabel('period (min)')
+xlim([1 100])
+ylim([0 0.01])
+set(gca, 'LineWidth', lw2, 'FontSize', font)
+grid on
+
+%% Plot time series
+
+font = 24; % fontsize
+lw1 = 3;
+lw2 = 2;
+
+init = 4459; 
+factor = 3; % corresponds to 24/factor hours
+t_len = L_block_ce/factor + 1; 
+
+figure('Units','normalized','Position',[.001 .001 .999 .999])
+subplot(211)
+plot(t_ce(1:t_len), f_ce(init: init+L_block_ce/factor), '.-', 'LineWidth', lw1)
+title('F_{CE}')
+ylabel('frequency (Hz)')
+xlim([0 24/factor*60])
+set(gca, 'LineWidth', lw2, 'FontSize', font)
+grid on
+
+subplot(212)
+plot(t_gb(1:t_len), f_gb(init: init+L_block_gb/factor),  '.-', 'LineWidth', lw1)
+title('F_{GB}')
+xlabel('time (min)')
+ylabel('frequency (Hz)')
+xlim([0 24/factor*60])
+set(gca, 'LineWidth', lw2, 'FontSize', font)
+grid on
